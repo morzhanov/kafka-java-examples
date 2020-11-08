9e5a36bd98a017a29637754b4d7003cdf510f827
vmorzhanov <vmorzhanov@ODL1902005.lohika.com>
2020-11-08 19:27:15 +0200

    added base code for connect app

---
+++ file-sync-connector/src/main/java/com/example/filesyncconnector/FileSyncConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesyncconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSyncConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSyncConnectorApplication.class, args);
+	}
+
+}
---
+++ file-sync-connector/src/test/java/com/example/filesyncconnector/FileSyncConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesyncconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSyncConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
